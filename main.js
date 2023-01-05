fca01590aa67cf5822fb3b5a4eedc573e7455f6f
nicolae-co <cojocarugithub@gmail.com>
2023-01-05 11:51:32 +0200

    Stars are created with javascript

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 const rating = document.querySelector('.rating');
-const stars = Array.from(document.querySelectorAll('.star'));
 
-rating.addEventListener('click', e => {
-  const clickedStar = e.target;
-  const clickedValue = clickedStar.dataset.value;
 
-  stars.forEach((star, i) => {
-    if (i+2 > clickedValue) {
-      star.classList.add('text-warning');
-    } else {
-      star.classList.remove('text-warning');
-    }
+window.onload = function () {
+  for (let i = 1; i < 6; i++) {
+    let star = document.createElement('span');
+    star.classList.add('star');
+    star.innerHTML = "&#9733;";
+    star.setAttribute('data-value', i)
+    rating.appendChild(star)
+  }
+  const stars = Array.from(document.querySelectorAll('.star'));
+
+  rating.addEventListener('click', e => {
+    const clickedStar = e.target;
+    const clickedValue = clickedStar.dataset.value;
+  
+    stars.forEach((star, i) => {
+      if (i+2 > clickedValue) {
+        star.classList.add('text-warning');
+      } else {
+        star.classList.remove('text-warning');
+      }
+    });
   });
-});
+  
+}
+
+
+
